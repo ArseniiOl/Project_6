a930cdf78ef798f9245150cc79056b013e6a8035
Arsenii-O <lenarseny222@gmail.com@>
2020-10-24 18:55:27 +0300

    Initial commit

---
+++ js/js.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    $('.slider').slick({
+        arrows: false,
+        dots: true,
+        adaptiveHeight: true,
+        speed: 500,
+        easing: 'ease,',
+        infinite: true,
+        initialSlide: 0,
+        autoPlay: true,
+        autoplaySpeed: 1500,
+    });
+
+});
